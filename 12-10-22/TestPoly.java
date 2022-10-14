6975c695140b30760771fafa47e4d628b65c1ba1
Pensionthieng Hrangchal <87723912+PesoHrangchal@users.noreply.github.com>
2022-10-14 10:13:04 +0530

    Create TestPoly.java

---
+++ 12-10-22/TestPoly.java
@@ -0,0 +1,32 @@
+package practical;
+class Bank{
+	float rateOfInterest() {
+		return 0;
+	}
+}
+class Axis extends Bank{
+	float rateOfInterest() {
+		return 2.3f;
+	}
+}
+class SBI extends Bank{
+	float rateOfInterest() {
+		return 7.3f;
+	}
+}
+class ICICI extends Bank{
+	float rateOfInterest() {
+		return 5.4f;
+	}
+}
+public class TestPoly {
+	public static void main(String args[]) {
+		Bank obj;//upcasting
+		obj=new Axis();
+		System.out.println("Axis "+obj.rateOfInterest());
+		obj=new SBI();
+		System.out.println("SBI "+obj.rateOfInterest());
+		obj=new ICICI();
+		System.out.println("ICICI "+obj.rateOfInterest());
+	}
+}
