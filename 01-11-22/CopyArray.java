1d7af8580cc377007167e4c15e479e731ce65536
Pensionthieng Hrangchal <87723912+PesoHrangchal@users.noreply.github.com>
2022-11-02 13:01:18 +0530

    Create CopyArray.java

---
+++ 01-11-22/CopyArray.java
@@ -0,0 +1,13 @@
+package array;
+
+public class CopyArray {//class
+	public static void main(String args[]) {
+		//declaring a source array
+		char[] copyFrom= {'d','e','c','f','f','e','i','n'};
+		//declaring a destination array
+		char[] copyTo=new char[7];//new array
+		System.arraycopy(copyFrom,2,copyTo,0,5);
+		//print the destination array
+		System.out.println(String.valueOf(copyTo));
+	}
+}
